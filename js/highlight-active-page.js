cddfda76f91eb3fcd6ccb264db728496712f49b7
Menemi <danila.titov1210@yandex.ru>
2023-08-31 09:09:18 +0300

    fix: smth

--- js/highlight-active-page.js
+++ js/highlight-active-page.js
@@ -1,5 +1,11 @@
 function handleLocationChange(location) {
     let element = document.querySelectorAll(".tab");
+    const homePage = "MenemiCV/";
+
+    if (location.replace(document.location.protocol, "").replace(document.location.host, "").replace("///", "") === homePage) {
+        element[0].classList.add("active");
+        return;
+    }
 
     for (let i = 0; i < element.length; i++) {
         if (location === element[i].href) {
